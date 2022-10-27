f613b5b2067a8f97c2db7672de3aa07869c4e647
Silas Kraume <silas.kraume1552@gmail.com>
2022-10-27 17:40:31 +0200

    Python 3.11 compatability

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,3 +1,5 @@
-__version__ = "1.0.6"
+"""Simple OS Independent 'cat' Command-line Tool made in Python."""
+
+__version__ = "1.0.7"
 __sysversion__ = "3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)]"
 __author__ = 'Silas A. Kraume'
