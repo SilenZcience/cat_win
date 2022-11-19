ac82757e2ac8a36dea346840b947430128c841b5
Silas Kraume <silas.kraume1552@gmail.com>
2022-11-19 15:32:50 +0100

    further optimization

--- cat_win/util/Holder.py
+++ cat_win/util/Holder.py
@@ -1,16 +1,19 @@
 from cat_win.util.ArgConstants import *
 from heapq import nlargest
 
+
 class Holder():
-    files = []
-    args = []
+    files = []  # all files, including tmp-file from stdin
+    args = [] # list of all used parameters: format [[id, param]]
     args_id = []
-    temp_file = None
+    temp_file = None # if stdin is used, this temp_file will contain the stdin-input
     reversed = False
-    maxlineLength = 0
-    lineSum = 0
-    fileLineMaxLength = 0
-    fileMaxLength = 0
+    
+    allFilesLinesSum = 0 
+    fileLineNumberPlaceHolder = 0
+    fileNumberPlaceHolder = 0
+    fileLineLengthPlaceHolder = 0 # the amount of chars neccessary to display the longest line within all files
+    
     clipBoard = ""
 
     def setFiles(self, files: list) -> None:
@@ -23,10 +26,10 @@ class Holder():
 
     def setTempFile(self, file: str) -> None:
         self.temp_file = file
-        
+
     def getAppliedFiles(self) -> list:
         return ["<STDIN>" if f == self.temp_file else f for f in self.files]
-    
+
     def __count_generator__(self, reader) -> bytes:
         b = reader(1024 * 1024)
         while b:
@@ -39,32 +42,30 @@ class Holder():
             count = sum(buffer.count(b'\n') for buffer in c_generator)
             return count + 1
 
-    def __calcFilesLineSum__(self) -> None:
-        self.lineSum = max([self.__getFileLinesSum__(file) for file in self.files])
-
-    def __calcFileLineMaxLength__(self) -> None:
-        self.fileLineMaxLength = len(str(self.lineSum))
-
-    def __calcFileMaxLength__(self) -> None:
-        self.fileMaxLength = len(str(len(self.files)))
+    def __calcPlaceHolder__(self) -> None:
+        fileLines = [self.__getFileLinesSum__(file)
+                           for file in self.files]
+        self.allFilesLinesSum = sum(fileLines)
+        self.fileLineNumberPlaceHolder = len(str(max(fileLines)))
+        self.fileNumberPlaceHolder = len(str(len(self.files)))
     
     def __calcMaxLine__(self, file):
         heap = []
         lines = []
         lines = open(file, "rb").readlines()
         heap = nlargest(1, lines, len)
-        if len(heap) == 0: return 0
+        if len(heap) == 0:
+            return 0
         longest_line = heap[0][:-1]
-        
+
         if longest_line.endswith(b'\r'):
             longest_line = longest_line[:-1]
         return len(str(max(len(longest_line), len(lines[-1]))))
 
-    def __calcMaxLineLength__(self) -> None:
-        self.maxlineLength = max(self.__calcMaxLine__(file) for file in self.files)
+    def __calcfileLineLengthPlaceHolder__(self) -> None:
+        self.fileLineLengthPlaceHolder = max(self.__calcMaxLine__(file)
+                                 for file in self.files)
 
     def generateValues(self) -> None:
-        self.__calcFilesLineSum__()
-        self.__calcFileLineMaxLength__()
-        self.__calcFileMaxLength__()
-        self.__calcMaxLineLength__()
+        self.__calcPlaceHolder__()
+        self.__calcfileLineLengthPlaceHolder__()
