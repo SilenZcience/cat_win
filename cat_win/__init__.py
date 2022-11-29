0ba4ce3019ca5ef2ad317054034dea7f6bf89aa0
Silas Kraume <silas.kraume1552@gmail.com>
2022-11-29 15:00:08 +0100

    option to create empty files

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,5 +1,5 @@
 """Simple OS Independent 'cat' Command-line Tool made in Python."""
 
-__version__ = "1.0.18"
+__version__ = "1.0.19"
 __sysversion__ = '3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
