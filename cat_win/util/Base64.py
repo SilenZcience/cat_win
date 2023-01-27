e3f6a3e2cad27a048885b59f9dbc9ebd33fe986d
Silas Kraume <silas.kraume1552@gmail.com>
2023-01-27 14:15:48 +0100

    Specify encoding in base64

--- cat_win/util/Base64.py
+++ cat_win/util/Base64.py
@@ -1,24 +1,28 @@
 from base64 import b64encode, b64decode
 
-def encodeBase64(content: list) -> list:
+def encodeBase64(content: list, encoding: str = None) -> list:
+    if encoding == None:
+        encoding = 'utf-8'
     # concatenate all lines and join them with line breaks
     contentLines = list(map(lambda x: x[1], content))
     contentLine = '\n'.join(contentLines)
     # encode the string to bytes and encode with base64
-    contentBytes = contentLine.encode()
+    contentBytes = contentLine.encode(encoding=encoding)
     base64_bytes = b64encode(contentBytes)
-    encoded_content = base64_bytes.decode('ascii')
+    encoded_content = base64_bytes.decode(encoding='ascii')
     # return as a single line
     return [('', encoded_content)]
 
-def decodeBase64(content: list) -> list:
+def decodeBase64(content: list, encoding: str = None) -> list:
+    if encoding == None:
+        encoding = 'utf-8'
     # concatenate all lines and join them
     contentLines = list(map(lambda x: x[1], content))
     contentLine = ''.join(contentLines)
     # encode the string to bytes and decode with base64
-    base64_bytes = contentLine.encode("ascii")
+    base64_bytes = contentLine.encode(encoding="ascii")
     decoded_content = b64decode(base64_bytes)
-    decoded_content = decoded_content.decode()
+    decoded_content = decoded_content.decode(encoding=encoding)
     # return as content list, split at line breaks
     decoded_content = decoded_content.split('\n')
     return [('', line) for line in decoded_content]
