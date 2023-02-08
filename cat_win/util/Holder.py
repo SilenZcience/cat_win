74f995bb3e577bfef4bc98805dd8f4c6ff0e0a4c
Silas Kraume <silas.kraume1552@gmail.com>
2023-02-08 11:20:24 +0100

    quickfix colored clip bug

--- cat_win/util/Holder.py
+++ cat_win/util/Holder.py
@@ -32,8 +32,11 @@ class Holder():
             self.args_id[id] = True
         if self.args_id[ARGS_B64E]:
             self.args_id[ARGS_NOCOL] = True
+            # prefix will be deleted anyway
             self.args_id[ARGS_LLENGTH] = False
             self.args_id[ARGS_NUMBER] = False
+        if self.args_id[ARGS_CLIP]:
+            self.args_id[ARGS_NOCOL] = True
         self.reversed = self.args_id[ARGS_REVERSE]
 
     def setTempFile(self, file: str) -> None:
