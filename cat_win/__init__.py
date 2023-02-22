102eec265da6e865d8d81c5f6430e9689ecbb2ef
Silas Kraume <silas.kraume1552@gmail.com>
2023-02-22 13:03:49 +0100

    new version release

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,5 +1,5 @@
 """Simple OS Independent 'cat' Command-line Tool made in Python."""
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __sysversion__ = '3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
