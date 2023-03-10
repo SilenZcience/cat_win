745ab3491a732d235ad1f0f2cc8b8290b01a4f41
Silas Kraume <silas.kraume1552@gmail.com>
2023-03-10 09:57:29 +0100

    added file sizes to -f

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,6 +1,6 @@
 """Simple OS Independent 'cat' Command-line Tool made in Python."""
 
-__version__ = '1.1.5'
+__version__ = '1.1.6'
 __sysversion__ = '3.11.2 (main, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/cat_win'
