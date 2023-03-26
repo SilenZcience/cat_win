1d7af3db1e2db1baf88b1bb5d39a78ce1c2ba60b
Silas Kraume <silas.kraume1552@gmail.com>
2023-03-26 13:49:49 +0200

    attemp fix stdinhelper test

--- cat_win/tests/test_StdInHelper.py
+++ cat_win/tests/test_StdInHelper.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 class TestStdInHelper(TestCase):
     def test_path_parts(self):
-        expected_output = ['C:/', 'a', 'b', 'c', 'd.txt']
+        expected_output_win = ['C:/', 'a', 'b', 'c', 'd.txt']
+        expected_output_unix_mac = ['a', 'b', 'c', 'd.txt']
         
-        self.assertListEqual(path_parts('C:/a/b/c/d.txt'), expected_output)
+        self.assertIn(path_parts('C:/a/b/c/d.txt'), [expected_output_win, expected_output_unix_mac])
