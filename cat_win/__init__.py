360f4f86251ce5ab84c8e1000eda4963ba89c3c0
Silas Kraume <silas.kraume1552@gmail.com>
2023-03-29 13:06:20 +0200

    prep new release

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,6 +1,6 @@
 """Simple OS Independent 'cat' Command-line Tool made in Python."""
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __sysversion__ = '3.11.2 (main, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/cat_win'
