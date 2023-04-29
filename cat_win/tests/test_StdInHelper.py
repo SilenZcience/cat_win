303eb9e5609c67bb1a68c3323aac3e685fad3b31
Silas Kraume <silas.kraume1552@gmail.com>
2023-04-29 18:33:28 +0200

    seperated mock class file

--- cat_win/tests/test_StdInHelper.py
+++ cat_win/tests/test_StdInHelper.py
@@ -1,32 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+from cat_win.tests.mocks.std import StdInMock
 from cat_win.util.StdInHelper import path_parts, getStdInContent
 
-class StdInMockIter:
-    def __init__(self, mock: object) -> None:
-        self.input_value = mock.input_value
-        self.splitted_input_value = self.input_value.split('\n')
-        self.index = -1
-    
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        if self.index < len(self.splitted_input_value)-1:
-            self.index += 1
-            return self.splitted_input_value[self.index] + '\n'
-        raise StopIteration
-
-class StdInMock:
-    def __init__(self, input_value: str) -> None:
-        self.input_value = input_value
-    
-    def readline(self) -> str:
-        return self.input_value.split('\n')[0] + '\n'
-    
-    def __iter__(self):
-        return StdInMockIter(self)
 
 class TestStdInHelper(TestCase):
     def test_path_parts(self):
