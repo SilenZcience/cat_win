9e4f711c5d88208e3be6371569b093537c3e9c1b
Silas Kraume <silas.kraume1552@gmail.com>
2023-09-16 00:07:04 +0200

    added tests for is_valid_uri

--- cat_win/tests/test_urls.py
+++ cat_win/tests/test_urls.py
@@ -3,9 +3,12 @@ from unittest import TestCase
 import random
 
 from cat_win.tests.mocks.error import ErrorDefGen
-from cat_win.util.urls import sep_valid_urls, read_url
+from cat_win.util.urls import is_valid_uri, sep_valid_urls, read_url
 
 class TestUrls(TestCase):
+    def test_is_valid_uri_valueerror(self):
+        self.assertEqual(is_valid_uri('invalid uri'), False)
+
     def test_sep_valid_urls(self):
         random.seed(1)
         valid_expected = [
