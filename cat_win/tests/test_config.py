cca82c2e06dd07290748c2152c2c443bab388aae
Silas Kraume <silas.kraume1552@gmail.com>
2024-01-16 00:30:47 +0100

    added tests for config

--- cat_win/tests/test_config.py
+++ cat_win/tests/test_config.py
@@ -2,38 +2,59 @@ from unittest.mock import patch
 from unittest import TestCase
 import os
 
-from cat_win.const.colorconstants import ColorOptions, CKW
+from cat_win.const.defaultconstants import DKW
 from cat_win.tests.mocks.std import StdOutMock
-from cat_win.persistence.cconfig import CConfig
+from cat_win.persistence.config import Config
 # import sys
 # sys.path.append('../cat_win')
 
 
 test_file_dir = os.path.join(os.path.dirname(__file__), 'texts')
-config = CConfig(test_file_dir)
+config = Config(test_file_dir)
 config.load_config()
 
 
 class TestConfig(TestCase):
     maxDiff = None
 
-    def test__print_get_all_available_colors(self):
-        all_color_options = list(ColorOptions.Fore.keys()) + list(ColorOptions.Back.keys())
-        all_color_options = [k for k in all_color_options if k != 'RESET']
+    def test_convert_config_element(self):
+        self.assertEqual(Config.convert_config_element('15', int), 15)
+        self.assertEqual(Config.convert_config_element('7.4', float), 7.4)
+        self.assertEqual(Config.convert_config_element('15', list), ['1', '5'])
+        self.assertEqual(Config.convert_config_element('false', bool), False)
+        self.assertEqual(Config.convert_config_element('FALSE', bool), False)
+        self.assertEqual(Config.convert_config_element('no', bool), False)
+        self.assertEqual(Config.convert_config_element('n', bool), False)
+        self.assertEqual(Config.convert_config_element('0', bool), False)
+        self.assertEqual(Config.convert_config_element('nfalse', bool), True)
+        self.assertEqual(Config.convert_config_element('1', bool), True)
 
-        with patch('cat_win.cat.sys.stdout', new=StdOutMock()) as fake_out:
-            config._print_get_all_available_colors()
-            for i, k in enumerate(all_color_options, start=1):
-                self.assertIn(str(i), fake_out.getvalue())
-                self.assertIn(k, fake_out.getvalue())
+    def test_is_valid_value(self):
+        self.assertEqual(Config.is_valid_value('', int), False)
+        self.assertEqual(Config.is_valid_value('a', int), False)
+        self.assertEqual(Config.is_valid_value('a', str), True)
+        self.assertEqual(Config.is_valid_value(5, float), True)
+        self.assertEqual(Config.is_valid_value(5, int), True)
+        self.assertEqual(Config.is_valid_value('True', bool), True)
+        self.assertEqual(Config.is_valid_value('TrUe', bool), True)
+        self.assertEqual(Config.is_valid_value('FaLSe', bool), True)
+        self.assertEqual(Config.is_valid_value('Yes', bool), True)
+        self.assertEqual(Config.is_valid_value('y', bool), True)
+        self.assertEqual(Config.is_valid_value('nO', bool), True)
+        self.assertEqual(Config.is_valid_value('n', bool), True)
+        self.assertEqual(Config.is_valid_value('0', bool), True)
+        self.assertEqual(Config.is_valid_value('1', bool), True)
+        self.assertEqual(Config.is_valid_value('tru', bool), False)
+
+    def test_load_config(self):
+        config.const_dic[DKW.DEFAULT_COMMAND_LINE] = '-nl "find= "'
+        self.assertListEqual(config.get_cmd(), ['-nl', 'find= '])
 
     def test__print_all_available_elements(self):
-        class_members = [attr for attr in dir(CKW) if not (
-            callable(getattr(CKW, attr)) or attr.startswith('__') or attr.startswith('RESET')
-        )]
+        all_elements = list(config.default_dic.keys())
 
         with patch('cat_win.cat.sys.stdout', new=StdOutMock()) as fake_out:
             config._print_all_available_elements()
-            for i, k in enumerate(class_members, start=1):
+            for i, k in enumerate(all_elements, start=1):
                 self.assertIn(str(i), fake_out.getvalue())
-                self.assertIn(getattr(CKW, k), fake_out.getvalue())
+                self.assertIn(k, fake_out.getvalue())
