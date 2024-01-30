9144d0ed30d603d51ff7700c4978cc9d33d1a37b
Silas Kraume <silas.kraume1552@gmail.com>
2024-01-30 14:44:44 +0100

    editor indentation config option

--- cat_win/const/defaultconstants.py
+++ cat_win/const/defaultconstants.py
@@ -11,3 +11,4 @@ class DKW:
     DEFAULT_FILE_ENCODING = 'default_file_encoding'
     LARGE_FILE_SIZE = 'large_file_size'
     STRIP_COLOR_ON_PIPE = 'strip_color_on_pipe'
+    EDITOR_INDENTATION = 'editor_indentation'
