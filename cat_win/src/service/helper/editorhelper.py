3279a51ca09a34f6d4005b025f4fdc63fd8997bd
Silas Kraume <silas.kraume1552@gmail.com>
2024-04-28 13:04:16 +0200

    define valid hex byte chars

--- cat_win/src/service/helper/editorhelper.py
+++ cat_win/src/service/helper/editorhelper.py
@@ -182,6 +182,10 @@ ACTION_STACKABLE = [
 # (e.g. when writing a word, the entire word should be undone/redone)
 
 
+HEX_BYTE_KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                 'A', 'B', 'C', 'D', 'E', 'F']
+
+
 class Position:
     """
     define a position in the text
