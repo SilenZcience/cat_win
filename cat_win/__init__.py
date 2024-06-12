9195e0cab0c36aa476740fcc6eadf89bf1915171
Silas Kraume <silas.kraume1552@gmail.com>
2024-06-12 02:47:17 +0200

    updated sysversion

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -2,6 +2,6 @@
 
 __project__ = 'cat_win'
 __version__ = '1.7.11'
-__sysversion__ = '3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)]'
+__sysversion__ = '3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/cat_win'
