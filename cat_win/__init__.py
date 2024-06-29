965f7b33544620aa63ddce6d7347c6db075f94de
Silas Kraume <silas.kraume1552@gmail.com>
2024-06-29 21:52:13 +0200

    bump version

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,7 +1,7 @@
 """Simple OS Independent 'cat' Command-line Tool made in Python."""
 
 __project__ = 'cat_win'
-__version__ = '1.7.11'
+__version__ = '1.7.12'
 __sysversion__ = '3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/cat_win'
