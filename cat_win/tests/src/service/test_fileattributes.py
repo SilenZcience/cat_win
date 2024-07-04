81e91448b3f2a80a31088ef24c4fced22bdcbb30
Silas Kraume <silas.kraume1552@gmail.com>
2024-07-04 22:45:19 +0200

    signatures test

--- cat_win/tests/src/service/test_fileattributes.py
+++ cat_win/tests/src/service/test_fileattributes.py
@@ -1,10 +1,29 @@
 from unittest import TestCase
 from unittest.mock import patch
+import os
 
 from cat_win.tests.mocks.std import StdOutMock
-from cat_win.src.service.fileattributes import _convert_size, get_file_meta_data, get_file_size, print_meta
+from cat_win.src.service.fileattributes import _convert_size, get_file_meta_data, get_file_size, print_meta, Signatures
 # import sys
 # sys.path.append('../cat_win')
+res_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'res')
+signatures_path = os.path.abspath(os.path.join(res_dir, 'signatures.json'))
+test_zip_file_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
+test_zip_file_path  = os.path.abspath(os.path.join(test_zip_file_dir, 'test.zip'))
+test_tar_file_path  = os.path.abspath(os.path.join(test_zip_file_dir, 'test.tar.gz'))
+
+
+class TestSignatures(TestCase):
+    def test_read_signature_failed(self):
+        self.assertEqual(Signatures.read_signature('', __file__), 'lookup failed!')
+        self.assertEqual(Signatures.read_signature(signatures_path, ''), 'lookup failed!')
+
+    def test_read_signatures_empty(self):
+        self.assertEqual(Signatures.read_signature(signatures_path, __file__), '-')
+
+    def test_read_signatures(self):
+        self.assertEqual(Signatures.read_signature(signatures_path, test_zip_file_path), 'application/zip(zip) [application/oxps(oxps);application/vnd.google-earth.kmz(kmz);application/vnd.kde.kword(kwd);application/vnd.ms-xpsdocument(xps);application/vnd.oasis.opendocument.presentation(odp);application/vnd.oasis.opendocument.text(odt);application/vnd.oasis.opendocument.text-template(ott);application/vnd.sun.xml.calc(sxc);application/vnd.sun.xml.draw(sxd);application/vnd.sun.xml.impress(sxi);application/vnd.sun.xml.writer(sxw);application/x-java-archive(jar);application/x-xpinstall(xpi)]')
+        self.assertEqual(Signatures.read_signature(signatures_path, test_tar_file_path), 'application/gzip(gz) [application/gzip(tgz)]')
 
 
 class TestFileAttributes(TestCase):
