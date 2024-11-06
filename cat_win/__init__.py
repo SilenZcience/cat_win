bf6219c75c5d614f5481b53ab6a95e687250cc36
Silas Kraume <silas.kraume1552@gmail.com>
2024-11-06 13:04:35 +0100

    bump version (pre release)

--- cat_win/__init__.py
+++ cat_win/__init__.py
@@ -1,7 +1,7 @@
 """Simple Text-Processing and -Analytics Command Line Tool made in Python."""
 
 __project__ = 'cat_win'
-__version__ = '1.9.2'
+__version__ = '1.10.0a'
 __sysversion__ = '3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)]'
 __author__ = 'Silas A. Kraume'
 __url__ = 'https://github.com/SilenZcience/cat_win'
