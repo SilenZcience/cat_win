285ce79a17e33d659322d0e51387e697f9d3a808
Silas Kraume <silas.kraume1552@gmail.com>
2024-12-13 20:48:13 +0100

    added clipboard tests

--- cat_win/tests/src/service/test_clipboard.py
+++ cat_win/tests/src/service/test_clipboard.py
@@ -14,6 +14,11 @@ test_file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'texts', 't
 class TestClipboard(TestCase):
     maxDiff = None
 
+    def test_clipabord_clear(self):
+        Clipboard.clipboard = '42'
+        Clipboard.clear()
+        self.assertEqual(Clipboard.clipboard, '')
+
     def test_put_cached(self):
         # the patch makes the copy_function not None
         with patch.object(Clipboard, 'copy_function') as mocka, patch.object(Clipboard, '_copy') as mockb:
@@ -36,6 +41,32 @@ class TestClipboard(TestCase):
     def test__copy_importerror(self):
         with patch('sys.stderr', new=StdOutMock()) as fake_out:
             Clipboard._copy('', 0, False)
+            self.assertNotIn('ClipBoardError', fake_out.getvalue())
+            self.assertIn('ImportError', fake_out.getvalue())
+            self.assertIn("'--clip'", fake_out.getvalue())
+
+    def test_get_cached(self):
+        # the patch makes the copy_function not None
+        with patch.object(Clipboard, 'paste_function') as mocka, patch.object(Clipboard, '_paste') as mockb:
+            Clipboard.get()
+        mocka.assert_called_once()
+        mockb.assert_not_called()
+
+    def test_get_not_cached(self):
+        with patch.object(Clipboard, '_paste') as mockb:
+            self.assertEqual(Clipboard.get(), None)
+        mockb.assert_called_once()
+
+    def test__paste_clipboarderror(self):
+        with patch('sys.stderr', new=StdOutMock()) as fake_out:
+            Clipboard._paste(0, True)
+            self.assertIn('ClipBoardError', fake_out.getvalue())
+            self.assertNotIn('ImportError', fake_out.getvalue())
+            self.assertIn("'--clip'", fake_out.getvalue())
+
+    def test__paste_importerror(self):
+        with patch('sys.stderr', new=StdOutMock()) as fake_out:
+            Clipboard._paste( 0, False)
             self.assertNotIn('ClipBoardError', fake_out.getvalue())
             self.assertIn('ImportError', fake_out.getvalue())
             self.assertIn("'--clip'", fake_out.getvalue())
