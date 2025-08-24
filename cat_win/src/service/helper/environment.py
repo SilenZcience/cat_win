af6bda1de95fdcace82b5a81c3bab5f9f71e7dc8
Silas Kraume <silas.kraume1552@gmail.com>
2025-08-25 00:12:02 +0200

    added environment tests

--- cat_win/src/service/helper/environment.py
+++ cat_win/src/service/helper/environment.py
@@ -18,8 +18,16 @@ def get_py_executable() -> str:
         the python executable used to start the current process
     """
     py_executable = sys.executable
-    if os.path.dirname(py_executable) in os.environ['PATH'].split(os.pathsep):
+    py_dirname    = os.path.normcase(os.path.normpath(os.path.dirname(py_executable)))
+    environ_paths = os.environ.get('PATH', '').split(os.pathsep)
+
+    def _paths_equal(p):
+        return os.path.normcase(os.path.normpath(p)) == py_dirname
+
+    if any(_paths_equal(p) for p in environ_paths):
         py_executable = os.path.basename(py_executable)
+        if on_windows_os and py_executable.lower().endswith('.exe'):
+            py_executable = py_executable[:-4]
     elif ' ' in py_executable:
         py_executable = f'"{py_executable}"' if on_windows_os else py_executable.replace(' ', '\\ ')
     return py_executable
