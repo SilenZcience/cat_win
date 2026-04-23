61f56cf4706a3a8c3f59e9c2fd0a5ee3e11f8f1e
Silas Kraume <silas.kraume1552@gmail.com>
2026-04-24 00:58:16 +0200

    fixed bug overlapping searches

--- cat_win/tests/src/service/test_querymanager.py
+++ cat_win/tests/src/service/test_querymanager.py
@@ -9,6 +9,7 @@ from cat_win.src.service.querymanager import (
     _map_display_pos,
     _build_ansi_restore,
     find_literals,
+    find_literals_no_overlap,
     find_regex,
     replace_queries_in_line
 )
@@ -43,70 +44,102 @@ class TestQueryManager(TestCase):
 
     def test_find_literals_true(self):
         _x = list(find_literals('test', 'abctEStdef', True))
-        self.assertEqual(_x, [[3, 7]])
+        self.assertListEqual(_x, [[3, 7]])
 
         _x = list(find_literals('TeSt', 'abctEstdef', True))
-        self.assertEqual(_x, [[3, 7]])
+        self.assertListEqual(_x, [[3, 7]])
 
         _x = list(find_literals('tesT', 'testabctestdetestf', True))
-        self.assertEqual(_x, [[0, 4], [7, 11], [13, 17]])
+        self.assertListEqual(_x, [[0, 4], [7, 11], [13, 17]])
 
         _x = list(find_literals('', '', True))
-        self.assertEqual(_x, [[0, 0]])
+        self.assertListEqual(_x, [[0, 0]])
 
         _x = list(find_literals('', 'x', True))
-        self.assertEqual(_x, [[0, 0], [1, 1]])
+        self.assertListEqual(_x, [[0, 0], [1, 1]])
+
+        _x = list(find_literals('aa', 'aaaaa', False))
+        self.assertListEqual(_x, [[0, 2], [1, 3], [2, 4], [3, 5]])
 
     def test_find_literals_false(self):
         _x = list(find_literals('a', '', False))
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
 
         _x = list(find_literals('TeSt', 'abctestdef', False))
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
 
         _x = list(find_literals('test', 'tsetabctesdeestf', False))
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
+
+    def test_find_literals_no_overlap_true(self):
+        _x = list(find_literals_no_overlap('test', 'abctEStdef', True))
+        self.assertListEqual(_x, [[3, 7]])
+
+        _x = list(find_literals_no_overlap('TeSt', 'abctEstdef', True))
+        self.assertListEqual(_x, [[3, 7]])
+
+        _x = list(find_literals_no_overlap('tesT', 'testabctestdetestf', True))
+        self.assertListEqual(_x, [[0, 4], [7, 11], [13, 17]])
+
+        _x = list(find_literals_no_overlap('', '', True))
+        self.assertListEqual(_x, [[0, 0]])
+
+        _x = list(find_literals_no_overlap('', 'x', True))
+        self.assertListEqual(_x, [[0, 0], [1, 1]])
+
+        _x = list(find_literals_no_overlap('aa', 'aaaaa', False))
+        self.assertListEqual(_x, [[0, 2], [2, 4]])
+
+    def test_find_literals_no_overlap_false(self):
+        _x = list(find_literals_no_overlap('a', '', False))
+        self.assertListEqual(_x, [])
+
+        _x = list(find_literals_no_overlap('TeSt', 'abctestdef', False))
+        self.assertListEqual(_x, [])
+
+        _x = list(find_literals_no_overlap('test', 'tsetabctesdeestf', False))
+        self.assertListEqual(_x, [])
 
     def test_find_regex_true(self):
         _x = list(find_regex(re.compile(r"test", re.DOTALL | re.IGNORECASE), 'TeSt'))
-        self.assertEqual(_x, [[0, 4]])
+        self.assertListEqual(_x, [[0, 4]])
 
     def test_find_regex_false(self):
         _x = list(find_regex(re.compile(r"[A-Z]{1}[a-z]+\s?.*\.+\s", re.DOTALL), 'silas A. Kraume'))
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
         _x = list(find_regex(re.compile(r"[0-9]{2}", re.DOTALL), '123'))
-        self.assertEqual(_x, [[0, 2]])
+        self.assertListEqual(_x, [[0, 2]])
 
         _x = list(find_regex(re.compile(r"[0-9]{2}", re.DOTALL), '1234'))
-        self.assertEqual(_x, [[0, 2], [2, 4]])
+        self.assertListEqual(_x, [[0, 2], [2, 4]])
 
         _x = list(find_regex(re.compile(r"[A-Z]{1}[a-z]*\s?.*\.+\s", re.DOTALL), 'Silas A. Kraume'))
-        self.assertEqual(_x, [[0, 9]])
+        self.assertListEqual(_x, [[0, 9]])
 
         _x = list(find_regex(re.compile(r"[A-Z]{1}[a-z]*\s?.*\.+\s", re.DOTALL), 'silas A. Kraume'))
-        self.assertEqual(_x, [[6, 9]])
+        self.assertListEqual(_x, [[6, 9]])
 
         _x = list(find_regex(re.compile(r"test", re.DOTALL), 'TeSt'))
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
 
     def test_optimize_intervals(self):
         string_finder = QueryManager([])
         _x = string_finder._optimize_intervals([[1, 3], [3, 5]])
-        self.assertEqual(_x, [[1, 5]])
+        self.assertListEqual(_x, [[1, 5]])
 
         _x = string_finder._optimize_intervals([[1, 10], [5, 10]])
-        self.assertEqual(_x, [[1, 10]])
+        self.assertListEqual(_x, [[1, 10]])
 
         _x = string_finder._optimize_intervals([[1, 3], [2, 5], [6, 8]])
-        self.assertEqual(_x, [[1, 5], [6, 8]])
+        self.assertListEqual(_x, [[1, 5], [6, 8]])
 
         _x = string_finder._optimize_intervals([[1, 3], [-5, 0], [0, 2]])
-        self.assertEqual(_x, [[-5, 3]])
+        self.assertListEqual(_x, [[-5, 3]])
 
     def test_optimize_intervals_empty(self):
         string_finder = QueryManager([])
         _x = string_finder._optimize_intervals([])
-        self.assertEqual(_x, [])
+        self.assertListEqual(_x, [])
 
     def test_find_keywords(self):
         string_finder = QueryManager([('Is', False),
